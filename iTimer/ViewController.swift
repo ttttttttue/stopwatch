af3a4ac452aae7dea8076f08846b847ff6f13df5
Anton <Krasikov-94@bk.ru>
2022-11-10 18:33:48 +0300

    Initial Commit

---
+++ iTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTimer
+//
+//  Created by Антон Красиков on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
